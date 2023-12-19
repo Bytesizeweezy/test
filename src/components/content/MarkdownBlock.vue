df5c7c4204a6c47d4693d799197111e49fbfc268
Bytesizeweezy <19474420+Bytesizeweezy@users.noreply.github.com>
2023-12-19 17:08:14 +0100

    Update MarkdownBlock.vue

--- src/components/content/MarkdownBlock.vue
+++ src/components/content/MarkdownBlock.vue
@@ -1,4 +1,4 @@
 <!-- This component is used to show Markdown code block examples -->
 <template>
-  <pre class="max-w-full overflow-x-auto"><ContentSlot :use="$slots.default" unwrap="p" /></pre>
+  <pre  ref="test3" class="max-w-full overflow-x-auto"><ContentSlot :use="$slots.default" unwrap="p"  ref="test4"/></pre>
 </template>
