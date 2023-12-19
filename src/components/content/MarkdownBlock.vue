ab8685268b0ba65bd29d471f9757a6dec420a899
Bytesizeweezy <19474420+Bytesizeweezy@users.noreply.github.com>
2023-12-19 17:29:03 +0100

    Delete src directory

--- src/components/content/MarkdownBlock.vue
+++
@@ -1,4 +0,0 @@
-<!-- This component is used to show Markdown code block examples -->
-<template>
-  <pre  ref="test3" class="max-w-full overflow-x-auto"><ContentSlot :use="$slots.default" unwrap="p"  ref="test4"/></pre>
-</template>
