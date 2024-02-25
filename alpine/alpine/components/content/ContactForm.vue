2c4f2450490b44c9fc20b4dfad35497d9e9c5ac5
Dustin <19474420+Bytesizeweezy@users.noreply.github.com>
2024-02-25 01:50:28 +0100

    copy Alpine theme and edit it

---
+++ alpine/alpine/components/content/ContactForm.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+const props = defineProps({
+  sendMailButtonText: {
+    type: String,
+    default: 'Per E-mail kontaktieren'
+  },
+  fields: {
+    type: Object as PropType<{email: String}>,
+    default: () => { '' }
+  }
+})
+</script>
+
+<template>
+    <div class="text">
+    E-mail: {{ fields.email }}
+    </div>
+    <div>
+    <Button>
+      <a :href="'mailto:' + fields.email">{{ sendMailButtonText }}</a>
+    </Button>
+    </div>
+</template>
+
+<style scoped lang="ts">
+css({
+  '.text': {
+    marginTop: '{space.8}',
+    marginBottom: '{space.2}',
+    fontSize: '{text.xl.fontSize}',
+    lineHeight: '{text.xl.lineHeight}',
+  }
+})
+</style>
